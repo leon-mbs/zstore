9c24d66a1792d7f03afbe398822fa9a3d5ed87b3
Leon <leon.mbs@gmail.com>
2019-07-08 22:27:59 +0300

    fix

--- db/update.sql
+++ db/update.sql
@@ -1,10 +1,11 @@
-CREATE TABLE `note_topics` (
-  `topic_id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(255) NOT NULL,
-  `content` text NOT NULL,
-  `favorites` tinyint(1) NOT NULL DEFAULT '0',
-  `ispublic` tinyint(1) DEFAULT '0',
-  PRIMARY KEY (`topic_id`)
+CREATE TABLE `note_nodes` (
+  `node_id` int(11) NOT NULL AUTO_INCREMENT,
+  `pid` int(11) NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `mpath` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`node_id`),
+  KEY `user_id` (`user_id`)
 )  DEFAULT CHARSET=utf8;
 
 CREATE TABLE `note_topicnode` (
@@ -22,31 +23,31 @@ CREATE TABLE `note_tags` (
   `tagvalue` varchar(255) NOT NULL,
   PRIMARY KEY (`tag_id`),
   KEY `topic_id` (`topic_id`)
-) DEFAULT CHARSET=utf8;
+)  DEFAULT CHARSET=utf8;
 
-CREATE TABLE `note_nodes` (
-  `node_id` int(11) NOT NULL AUTO_INCREMENT,
-  `pid` int(11) NOT NULL,
-  `title` varchar(50) NOT NULL,
-  `mpath` varchar(255) CHARACTER SET latin1 NOT NULL,
-  `user_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`node_id`),
-  KEY `user_id` (`user_id`)
-) DEFAULT CHARSET=utf8;
+CREATE TABLE `note_topics` (
+  `topic_id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `content` text NOT NULL,
+  `favorites` tinyint(1) NOT NULL DEFAULT '0',
+  `ispublic` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`topic_id`)
+)  DEFAULT CHARSET=utf8;
 
-CREATE   VIEW `topicnodeview` AS 
+CREATE  VIEW `note_topicnodeview` AS
   select 
-    `topicnode`.`topic_id` AS `topic_id`,
-    `topicnode`.`node_id` AS `node_id`,
-    `topicnode`.`tn_id` AS `tn_id`,
-    `topics`.`title` AS `title`,
-    `nodes`.`user_id` AS `user_id`,
-    `topics`.`content` AS `content` 
+    `note_topicnode`.`topic_id` AS `topic_id`,
+    `note_topicnode`.`node_id` AS `node_id`,
+    `note_topicnode`.`tn_id` AS `tn_id`,
+    `note_topics`.`title` AS `title`,
+    `note_nodes`.`user_id` AS `user_id`,
+    `note_topics`.`content` AS `content` 
   from 
-    ((`topics` join `topicnode` on((`topics`.`topic_id` = `topicnode`.`topic_id`))) join `nodes` on((`nodes`.`node_id` = `topicnode`.`node_id`)));
+    ((`note_topics` join `note_topicnode` on((`note_topics`.`topic_id` = `note_topicnode`.`topic_id`))) join `note_nodes` on((`note_nodes`.`node_id` = `note_topicnode`.`node_id`)));
 
 
-CREATE   VIEW `topicsview` AS 
+	
+CREATE     VIEW `note_topicsview` AS
   select 
     `t`.`topic_id` AS `topic_id`,
     `t`.`title` AS `title`,
@@ -55,24 +56,21 @@ CREATE   VIEW `topicsview` AS
     `t`.`ispublic` AS `ispublic`,
     `tn`.`user_id` AS `user_id` 
   from 
-    (`topics` `t` join `topicnodeview` `tn` on((`t`.`topic_id` = `tn`.`topic_id`)));
-
+    (`note_topics` `t` join `note_topicnodeview` `tn` on((`t`.`topic_id` = `tn`.`topic_id`)));
 	
-CREATE   VIEW note_nodesview AS 
+CREATE   VIEW `note_nodesview` AS
   select 
-    note_nodes.node_id AS node_id,
-    note_nodes.pid AS pid,
-    note_nodes.title AS title,
-    note_nodes.mpath AS mpath,
-    note_nodes.user_id AS user_id,
+    `note_nodes`.`node_id` AS `node_id`,
+    `note_nodes`.`pid` AS `pid`,
+    `note_nodes`.`title` AS `title`,
+    `note_nodes`.`mpath` AS `mpath`,
+    `note_nodes`.`user_id` AS `user_id`,
     (
   select 
-    count(note_topicnode.topic_id) AS Count(topic_id) 
+    count(`note_topicnode`.`topic_id`) AS `Count(topic_id)` 
   from 
-    note_topicnode 
+    `note_topicnode` 
   where 
-    (note_topicnode.node_id = note_nodes.node_id)) AS tcnt 
+    (`note_topicnode`.`node_id` = `note_nodes`.`node_id`)) AS `tcnt` 
   from 
-    note_nodes;	
-	
-	
+    `note_nodes`;
