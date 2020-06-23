24167bce4f852fdaaced07da75ee99183ec72e33
Leon <leon-mbs@users.noreply.github.com>
2020-06-23 23:51:05 +0300

    fix email

--- www/app/entity/doc/document.php
+++ www/app/entity/doc/document.php
@@ -737,16 +737,18 @@ class Document extends \ZCL\DB\Entity
             file_put_contents($f, $data);
 
             $mail = new \PHPMailer\PHPMailer\PHPMailer();
-            $mail->isSMTP();
-            $mail->Host = $_config['smtp']['host'];
-            $mail->Port = $_config['smtp']['port'];
-            $mail->Username = $_config['smtp']['user'];
-            $mail->Password = $_config['smtp']['pass'];
-            $mail->SMTPAuth = true;
-            if ($_config['smtp']['tls'] == 'true') {
-                $mail->SMTPSecure = \PHPMailer\PHPMailer\PHPMailer::ENCRYPTION_STARTTLS;
+            if($_config['smtp']['usesmtp']=='true') {
+                $mail->isSMTP();
+                $mail->Host = $_config['smtp']['host'];
+                $mail->Port = $_config['smtp']['port'];
+                $mail->Username = $_config['smtp']['user'];
+                $mail->Password = $_config['smtp']['pass'];
+                $mail->SMTPAuth = true;
+                if ($_config['smtp']['tls'] == 'true') {
+                    $mail->SMTPSecure = \PHPMailer\PHPMailer\PHPMailer::ENCRYPTION_STARTTLS;
+                }
+            
             }
-
             $mail->setFrom($_config['smtp']['user'], '');
             $mail->addAddress($customer->email);
             $mail->Subject = $doc->getEmailSubject();
