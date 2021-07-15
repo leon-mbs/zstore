16b7fb69570866048b53ebac524dc2fc4002ebcd
HOME\leonm <leon.mbs@gmail.com>
2021-07-15 18:43:23 +0300

    next

--- www/app/pages/service/discounts.php
+++ www/app/pages/service/discounts.php
@@ -33,6 +33,62 @@ class Discounts extends \App\Pages\Base
         if (false == \App\ACL::checkShowSer('Discounts')) {
             return;
         }
- 
+        $this->add(new ClickLink('tabo', $this, 'onTab'));
+        $this->add(new ClickLink('tabc', $this, 'onTab'));
+        $this->add(new ClickLink('tabi', $this, 'onTab'));
+        $this->add(new ClickLink('tabg', $this, 'onTab'));
+        $this->add(new ClickLink('tabs', $this, 'onTab'));
+        $this->add(new Panel('otab'));
+        $this->add(new Panel('ctab'));
+        $this->add(new Panel('itab'));
+        $this->add(new Panel('gtab'));
+        $this->add(new Panel('stab'));
+
+        $this->onTab($this->tabo);
+
+        $disc = System::getOptions("discount");
+        if (!is_array($disc)) {
+            $disc = array( );
+        }
+      
+        $form = $this->otab->add(new  Form("commonform")) ;
+        $form->onSubmit($this,"onCommon") ;
+        $form->add(new  TextInput("firstbay",$disc["firstbay"])) ;
+        $form->add(new  TextInput("bonus1",$disc["bonus1"])) ;
+        $form->add(new  TextInput("level2",$disc["level2"])) ;
+        $form->add(new  TextInput("bonus2",$disc["bonus2"])) ;
+        
+        
      }
+     
+     
+   public function onCommon($sender) {
+        $disc = System::getOptions("discount");
+        if (!is_array($disc)) {
+            $disc = array( );
+        }
+        $disc["firstbay"] =  $sender->firstbay->getText();
+        $disc["bonus1"] =  $sender->bonus1->getText();
+        $disc["level2"] =  $sender->level2->getText();
+        $disc["bonus2"] =  $sender->bonus2->getText();
+        System::setOptions("discount",$disc) ;
+        
+   }
+   
+   public function onTab($sender) {
+
+        $this->_tvars['tabcbadge'] = $sender->id == 'tabc' ? "badge badge-dark  badge-pill " : "badge badge-light  badge-pill  ";
+        $this->_tvars['tabobadge'] = $sender->id == 'tabo' ? "badge badge-dark  badge-pill " : "badge badge-light  badge-pill  ";;
+        $this->_tvars['tabibadge'] = $sender->id == 'tabi' ? "badge badge-dark  badge-pill " : "badge badge-light  badge-pill  ";;
+        $this->_tvars['tabgbadge'] = $sender->id == 'tabg' ? "badge badge-dark  badge-pill " : "badge badge-light  badge-pill  ";;
+        $this->_tvars['tabsbadge'] = $sender->id == 'tabs' ? "badge badge-dark  badge-pill " : "badge badge-light  badge-pill  ";;
+
+        $this->ctab->setVisible($sender->id == 'tabc');
+        $this->otab->setVisible($sender->id == 'tabo');
+        $this->itab->setVisible($sender->id == 'tabi');
+        $this->gtab->setVisible($sender->id == 'tabg');
+        $this->stab->setVisible($sender->id == 'tabs');
+        
+    }
+     
 }
