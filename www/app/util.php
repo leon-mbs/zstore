78409497fb86fce6a426e5866a4fb486508e7e12
leon.mbs@gmail.com <leon.mbs@gmail.com>
2021-10-01 20:44:29 +0300

    remove bcmath

--- www/app/util.php
+++ www/app/util.php
@@ -176,7 +176,7 @@ define('M2S_KOPS_MANDATORY', 0x02);    // mandatory copecks
 define('M2S_KOPS_SHORT', 0x04);    // shorten copecks
 
 function money2str_rugr($money, $options = 0) {
-
+             
     $money = preg_replace('/[\,\-\=]/', '.', $money);
 
     $numbers_m = array('', 'один', 'два', 'три', 'четыре', "пять", 'шесть', 'семь',
@@ -557,11 +557,17 @@ function money2str_ua($money, $options = 0) {
 
 // service function to select the group of digits
 function dec_digits_group($number, $power, $digits = 1) {
-    return (int)bcmod(bcdiv($number, bcpow(10, $power * $digits, 8)), bcpow(10, $digits, 8));
+    
+    
+      
+     return     intval(   ( $number/pow(10, $power * $digits) ) % pow(10,    $digits) ) ;
+    
+    
+   // return (int)bcmod(bcdiv($number, bcpow(10, $power * $digits, 8)), bcpow(10, $digits, 8));
 }
 
 // service function to get plural form for the number
-function sk_plural_form($d) {
+function sk_plural_form($d) {    
     $d = $d % 100;
     if ($d > 20) {
         $d = $d % 10;
@@ -575,63 +581,4 @@ function sk_plural_form($d) {
     }
 }
 
-/*
-  function num2str($num)
-  {
-  $nul = 'ноль';
-  $ten = array(
-  array('', 'один', 'два', 'три', 'четыре', 'пять', 'шесть', 'семь', 'восемь', 'девять'),
-  array('', 'одна', 'две', 'три', 'четыре', 'пять', 'шесть', 'семь', 'восемь', 'девять'),
-  );
-  $a20 = array('десять', 'одиннадцать', 'двенадцать', 'тринадцать', 'четырнадцать', 'пятнадцать', 'шестнадцать', 'семнадцать', 'восемнадцать', 'девятнадцать');
-  $tens = array(2 => 'двадцать', 'тридцать', 'сорок', 'пятьдесят', 'шестьдесят', 'семьдесят', 'восемьдесят', 'девяносто');
-  $hundred = array('', 'сто', 'двести', 'триста', 'четыреста', 'пятьсот', 'шестьсот', 'семьсот', 'восемьсот', 'девятьсот');
-  $unit = array(// Units
-  array('копейка', 'копейки', 'копеек', 1),
-  array('рубль', 'рубля', 'рублей', 0),
-  array('тысяча', 'тысячи', 'тысяч', 1),
-  array('миллион', 'миллиона', 'миллионов', 0),
-  array('миллиард', 'милиарда', 'миллиардов', 0),
-  );
-  //
-  list($rub, $kop) = explode('.', sprintf("%015.2f", floatval($num)));
-  $out = array();
-  if (intval($rub) > 0) {
-  foreach (str_split($rub, 3) as $uk => $v) { // by 3 symbols
-  if (!intval($v))
-  continue;
-  $uk = sizeof($unit) - $uk - 1; // unit key
-  $gender = $unit[$uk][3];
-  list($i1, $i2, $i3) = array_map('intval', str_split($v, 1));
-  // mega-logic
-  $out[] = $hundred[$i1]; # 1xx-9xx
-  if ($i2 > 1)
-  $out[] = $tens[$i2] . ' ' . $ten[$gender][$i3];# 20-99
-  else
-  $out[] = $i2 > 0 ? $a20[$i3] : $ten[$gender][$i3];# 10-19 | 1-9
-  // units without rub & kop
-  if ($uk > 1)
-  $out[] = morph($v, $unit[$uk][0], $unit[$uk][1], $unit[$uk][2]);
-  } //foreach
-  }
-  else
-  $out[] = $nul;
-  $out[] = morph(intval($rub), $unit[1][0], $unit[1][1], $unit[1][2]); // rub
-  $out[] = $kop . ' ' . morph($kop, $unit[0][0], $unit[0][1], $unit[0][2]); // kop
-  return trim(preg_replace('/ {2,}/', ' ', join(' ', $out)));
-  }
-
-
-  function morph($n, $f1, $f2, $f5)
-  {
-  $n = abs(intval($n)) % 100;
-  if ($n > 10 && $n < 20)
-  return $f5;
-  $n = $n % 10;
-  if ($n > 1 && $n < 5)
-  return $f2;
-  if ($n == 1)
-  return $f1;
-  return $f5;
-  }
- */
+ 
