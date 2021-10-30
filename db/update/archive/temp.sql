bcdf2c0ee143e5fe24f3414fff46e7fd90048518
leon.mbs@gmail.com <leon.mbs@gmail.com>
2021-10-30 17:37:34 +0300

    var

---
+++ db/update/archive/temp.sql
@@ -0,0 +1,18 @@
+CREATE TABLE shop_vars (
+  var_id int(11) NOT NULL AUTO_INCREMENT,
+  attr_id int(11) NOT NULL,
+  varname varchar(255) DEFAULT NULL,
+  PRIMARY KEY (var_id)
+ 
+  
+)
+ENGINE = innodb;
+
+
+CREATE TABLE  shop_vars (
+  var_id int(11) NOT NULL AUTO_INCREMENT,
+  attr_id int(11) NOT NULL,
+  varname varchar(255) DEFAULT NULL,
+  PRIMARY KEY (var_id)
+)
+ENGINE = innodb ; 
