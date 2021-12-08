c53ee410861fc3e9a4934742e56581747cd9c8b8
leon.mbs@gmail.com <leon.mbs@gmail.com>
2021-12-08 11:44:30 +0200

    fix
    возврат к  vue2

---
+++ www/assets/js/vue/paginator.vue
@@ -0,0 +1,24 @@
+<template>
+<nav  >
+  <ul class="pagination">
+    <li class="page-item "><a class="page-link" href="#">Previous</a></li>
+    <li class="page-item"><a class="page-link" href="#">1</a></li>
+    <li class="page-item"><a class="page-link" href="#">2</a></li>
+    <li class="page-item"><a class="page-link" href="#">3</a></li>
+    <li class="page-item"><a class="page-link" href="#">Next</a></li>
+  </ul>
+</nav>
+</template>
+
+<script>
+module.exports = {
+    data: function() {
+        return {
+            who: 'world'
+        }
+    } ,
+    props:['pr']
+}
+</script>
+
+ 
