0100f1ed9fb7e2a93564c4d34cbe552ccb1e87f7
leon.mbs@gmail.com <leon.mbs@gmail.com>
2021-12-27 11:52:45 +0200

    отчеты по  возвратам

---
+++ db/update/archive/temp.sql
@@ -0,0 +1,2 @@
+INSERT INTO `metadata` ( `meta_type`, `description`, `meta_name`, `menugroup`, `disabled`) VALUES( 2, 'Возвраты  покупателей', 'Returnselled', 'Продажи', 0);
+INSERT INTO `metadata` ( `meta_type`, `description`, `meta_name`, `menugroup`, `disabled`) VALUES( 2, 'Возвраты  поставщикам', 'Returnbayed', 'Закупки', 0);
