edabc59263c92973993bdc8d9c670707648469d1
leon.mbs@gmail.com <leon.mbs@gmail.com>
2022-06-11 20:14:22 +0300

    6.2.0

--- mysqldb/update/update610to620.sql
+++ mysqldb/update/update610to620.sql
@@ -1,29 +1,31 @@
-ALTER TABLE `ppo_zformstat` ADD `fiscnumber` VARCHAR(255) NULL ;
+
 
 ALTER TABLE `item_set` ADD `service_id` INT NULL  , ADD `cost` DECIMAL(10,2) NULL  ;
 
 
 DROP VIEW item_set_view  ;
 
-CREATE
+CREATE 
+    
+VIEW item_set_view
 AS
-SELECT
-  `item_set`.`set_id` AS `set_id`,
-  `item_set`.`item_id` AS `item_id`,
-  `item_set`.`pitem_id` AS `pitem_id`,
-  `item_set`.`qty` AS `qty`,
-  `item_set`.`service_id` AS `service_id`,
-  `item_set`.`cost` AS `cost`,
-  `items`.`itemname` AS `itemname`,
-  `items`.`item_code` AS `item_code`,
-  `services`.`service_name` AS `service_name`
-FROM ((`item_set`
-  LEFT JOIN `items`
-    ON (((`item_set`.`item_id` = `items`.`item_id`)
-    AND (`items`.`disabled` <> 1))))
-  LEFT JOIN `services`
-    ON (((`item_set`.`service_id` = `services`.`service_id`)
-    AND (`services`.`disabled` <> 1))));
+    SELECT
+      `item_set`.`set_id` AS `set_id`,
+      `item_set`.`item_id` AS `item_id`,
+      `item_set`.`pitem_id` AS `pitem_id`,
+      `item_set`.`qty` AS `qty`,
+      `item_set`.`service_id` AS `service_id`,
+      `item_set`.`cost` AS `cost`,
+      `items`.`itemname` AS `itemname`,
+      `items`.`item_code` AS `item_code`,
+      `services`.`service_name` AS `service_name`
+    FROM ((`item_set`
+      LEFT JOIN `items`
+        ON (((`item_set`.`item_id` = `items`.`item_id`)
+        AND (`items`.`disabled` <> 1))))
+      LEFT JOIN `services`
+        ON (((`item_set`.`service_id` = `services`.`service_id`)
+        AND (`services`.`disabled` <> 1))));
     
     
 ALTER TABLE `documents` ADD `lastupdate` DATETIME NULL;
@@ -67,3 +69,5 @@ FROM (((((`documents` `d`
   LEFT JOIN `firms` `f`
     ON ((`d`.`firm_id` = `f`.`firm_id`)));    
     
+    
+ALTER TABLE `ppo_zformstat` ADD `fiscnumber` VARCHAR(255) NULL ;    
