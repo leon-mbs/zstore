46f703b6c33f934312dcb4450e27b3bfc4de7e61
leon.mbs@gmail.com <leon.mbs@gmail.com>
2022-09-22 19:36:17 +0300

    hotfix

--- www/app/entity/doc/document.php
+++ www/app/entity/doc/document.php
@@ -560,7 +560,8 @@ class Document extends \ZCL\DB\Entity
     }
 
     public function nextNumber($branch_id = 0) {
-
+        $doc = $this->cast();
+ 
         $conn = \ZDB\DB::getConnect();
         $branch = "";
         if ($this->branch_id > 0) {
@@ -569,19 +570,27 @@ class Document extends \ZCL\DB\Entity
         if ($branch_id > 0) {
             $branch = " and branch_id=" . $branch_id;
         }
-        $d = Document::getFirst(" meta_id='{$this->meta_id}'   {$branch} ", " document_id desc")  ;
-        if($d == null)  {
-            $prevnumber =  $this->getNumberTemplate();
+         $limit =" limit 0,1";
+            if($conn->dataProvider=="postgres") {
+                $limit =" limit 1";
+            }  
+        $sql = "select document_number from  documents  where   meta_id='{$this->meta_id}'   {$branch}  order  by document_id desc ".$limit;
+        $prevnumber = $conn->GetOne($sql);
+        if (strlen($prevnumber) == 0) {
+            $prevnumber = $doc->getNumberTemplate();
         } else {
-            $prevnumber = $d->document_number;             
+ //           $prevnumber = $d->document_number;             
         }
         $letter = preg_replace('/[0-9]/', '', $prevnumber);
         $letter = $conn->qstr($letter.'%');
-        $d = Document::getFirst(" document_number like {$letter}     {$branch} ", " document_id desc")  ;
-        if($d == null)  {
-            $prevnumber =  $this->getNumberTemplate();
+
+        $sql = "select document_number from  documents  where   document_number like {$letter}     {$branch}  order  by document_id desc ".$limit;
+        $prevnumber = $conn->GetOne($sql);
+           
+        if (strlen($prevnumber) == 0) {
+            $prevnumber =  $doc->getNumberTemplate();
         } else {
-            $prevnumber = $d->document_number;             
+//            $prevnumber = $d->document_number;             
         }
 
         
--- www/app/pages/register/orderlist.php
+++ www/app/pages/register/orderlist.php
@@ -109,6 +109,7 @@ class OrderList extends \App\Pages\Base
         $row->add(new Label('number', $n));
         $row->add(new Label('date', H::fd($doc->document_date)));
         $row->add(new Label('onotes', $doc->notes));
+        $row->add(new Label('emp', $doc->username));
         $row->add(new Label('customer', $doc->customer_name));
         $row->add(new Label('amount', H::fa($doc->amount)));
         $stname = Document::getStateName($doc->state);
