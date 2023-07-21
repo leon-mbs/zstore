486d88cdc0201e9ea6074d9ca57e468332132639
leon.mbs@gmail.com <leon.mbs@gmail.com>
2023-07-21 16:14:43 +0300

    fix  style

--- www/app/widgets/catitemlist.php
+++ www/app/widgets/catitemlist.php
@@ -7,15 +7,12 @@ use App\Entity\Item;
 use App\Helper as H;
 use Zippy\Html\Label;
 use Zippy\Html\Panel;
- 
 
 /**
- * Виджет для подбора  товаров через  картинки 
+ * Виджет для подбора  товаров через  картинки
  */
 class CatItemList extends \Zippy\Html\PageFragment
 {
-
- 
     private $_food = false;
 
     /**
@@ -26,113 +23,113 @@ class CatItemList extends \Zippy\Html\PageFragment
      * @param mixed $event
      * @param mixed $pricetype
      */
-    public function __construct($id,$food = false ) {
+    public function __construct($id, $food = false) {
         parent::__construct($id);
         $this->_food = $food;
-        
+
         $this->add(new Label('_catitemlist_')) ;
-  
-  
-  
+
+
+
     }
- 
-   public function init( ) {
- 
-  
+
+    public function init() {
+
+
         $path = $this->id;
         $owner =  $this->getOwner() ;
-        while(($owner    instanceof \Zippy\Html\WebPage )==false) {
-             $path = $owner->id.'::'.$path ;
-             $owner =  $owner->getOwner() ; 
-        }                                   
-        $this->_catitemlist_->setAttribute('path',$path);  
-  
+        while(($owner    instanceof \Zippy\Html\WebPage)==false) {
+            $path = $owner->id.'::'.$path ;
+            $owner =  $owner->getOwner() ;
+        }
+        $this->_catitemlist_->setAttribute('path', $path);
+
     }
- 
- 
-      public function loaddata($args,$post=null) {
-          $post = json_decode($post)   ;
-          
-          $ret=[];
-          $ret['cats'] = [];
-          $ret['items']= [];
-          $ret['prev'] = 0;
-          if($args[0] > 0) {
-              $cat = Category::load($args[0]) ;
-              $ret['prev'] = (int)$cat->parent_id ;
-          }
-          
-          $where= "coalesce(parent_id,0)=".$args[0] ;
-          if($this->_food == true) {
-              $where .= " and detail  not  like '%<nofastfood>1</nofastfood>%' ";
-              
-          }
-          
-          $catlist = Category::find($where,"cat_name");
-          if(count($catlist) > 0) {
-              
-              foreach($catlist as $cat) {  
-                  $ret['cats'][] = array(
-                    'cat_id'=>$cat->cat_id,
-                    'cat_name'=>$cat->cat_name,
-                    'image'=>"/loadimage.php?id=" . $cat->image_id
-                  ) ;      
-              }
-              
-          }   else {
-              $prodlist = Item::find('disabled<>1  and  item_type in (1,4 )  and cat_id=' . $args[0] );
-              
-                foreach($prodlist as $prod) {  
-                  $ret['items'][] = array(
-                    'item_id'=>$prod->item_id,
-                    'itemname'=>$prod->itemname,
-                    'price'=>$prod->getPrice(),
-                    'image'=>"/loadimage.php?id=" . $prod->image_id
-                  ) ;      
-                }            
-              
-              
-          }
-          return json_encode($ret, JSON_UNESCAPED_UNICODE);     
-      }
-          
-      public function loaditems($args,$post=null) {
-          $post = json_decode($post)   ;
-          
-          $where = "disabled <> 1 ";
-          if(strlen($post->searchkey) > 0) {
-              $_sk= Item::qstr($post->searchkey);
-              $_skn= Item::qstr('%'.$post->searchkey.'%');
-                  
-              $where = $where. " and ( itemname  like {$_skn}  or item_code= {$_sk} or bar_code= {$_sk}  ) " ;
-          }
-          
-          if($post->searchcat > 0){
-             $where = $where. " and cat_id= ". $post->searchcat; 
-          }
-          if(strlen($post->searchbrand) > 0) {
-             $where = $where. " and manufacturer = ". Item::qstr($post->searchbrand);  
-          }
-          
-          $all =  Item::findCnt($where) ;
-          $items = [];
-          
-          foreach(Item::find($where,'itemname asc',$post->pagesize, $post->currpage * $post->pagesize  )  as $it) {
-              $items[]= array(
-               'item_id'=>$it->item_id,
-               'itemname'=>$it->itemname,
-               'item_code'=>$it->item_code,
-               'bar_code'=>$it->bar_code,
-               'brand'=>$it->manufacturer
-              );
-              
-           
-          }
-          
-          
-          return json_encode(array('items'=>$items,'rowscnt'=>$all), JSON_UNESCAPED_UNICODE);     
-          
+
+
+    public function loaddata($args, $post=null) {
+        $post = json_decode($post)   ;
+
+        $ret=[];
+        $ret['cats'] = [];
+        $ret['items']= [];
+        $ret['prev'] = 0;
+        if($args[0] > 0) {
+            $cat = Category::load($args[0]) ;
+            $ret['prev'] = (int)$cat->parent_id ;
+        }
+
+        $where= "coalesce(parent_id,0)=".$args[0] ;
+        if($this->_food == true) {
+            $where .= " and detail  not  like '%<nofastfood>1</nofastfood>%' ";
+
+        }
+
+        $catlist = Category::find($where, "cat_name");
+        if(count($catlist) > 0) {
+
+            foreach($catlist as $cat) {
+                $ret['cats'][] = array(
+                  'cat_id'=>$cat->cat_id,
+                  'cat_name'=>$cat->cat_name,
+                  'image'=>"/loadimage.php?id=" . $cat->image_id
+                ) ;
+            }
+
+        } else {
+            $prodlist = Item::find('disabled<>1  and  item_type in (1,4 )  and cat_id=' . $args[0]);
+
+            foreach($prodlist as $prod) {
+                $ret['items'][] = array(
+                  'item_id'=>$prod->item_id,
+                  'itemname'=>$prod->itemname,
+                  'price'=>$prod->getPrice(),
+                  'image'=>"/loadimage.php?id=" . $prod->image_id
+                ) ;
+            }
+
+
+        }
+        return json_encode($ret, JSON_UNESCAPED_UNICODE);
     }
- 
- 
+
+    public function loaditems($args, $post=null) {
+        $post = json_decode($post)   ;
+
+        $where = "disabled <> 1 ";
+        if(strlen($post->searchkey) > 0) {
+            $_sk= Item::qstr($post->searchkey);
+            $_skn= Item::qstr('%'.$post->searchkey.'%');
+
+            $where = $where. " and ( itemname  like {$_skn}  or item_code= {$_sk} or bar_code= {$_sk}  ) " ;
+        }
+
+        if($post->searchcat > 0) {
+            $where = $where. " and cat_id= ". $post->searchcat;
+        }
+        if(strlen($post->searchbrand) > 0) {
+            $where = $where. " and manufacturer = ". Item::qstr($post->searchbrand);
+        }
+
+        $all =  Item::findCnt($where) ;
+        $items = [];
+
+        foreach(Item::find($where, 'itemname asc', $post->pagesize, $post->currpage * $post->pagesize)  as $it) {
+            $items[]= array(
+             'item_id'=>$it->item_id,
+             'itemname'=>$it->itemname,
+             'item_code'=>$it->item_code,
+             'bar_code'=>$it->bar_code,
+             'brand'=>$it->manufacturer
+            );
+
+
+        }
+
+
+        return json_encode(array('items'=>$items,'rowscnt'=>$all), JSON_UNESCAPED_UNICODE);
+
+    }
+
+
 }
