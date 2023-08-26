222c16ae10d1be3e305c3d8616a1c87e6c957294
leon.mbs@gmail.com <leon.mbs@gmail.com>
2023-08-26 18:33:03 +0300

    add sql

--- mysqldb/update/temp.sql
+++ mysqldb/update/temp.sql
@@ -1,3 +1,27 @@
 ALTER TABLE `users` ADD `lastlogin` DATETIME NULL  ;
 
+CREATE TABLE zstore.departments (
+  department_id int(11) NOT NULL AUTO_INCREMENT,
+  department_name varchar(255) NOT NULL,
+  details text NOT NULL,
+  PRIMARY KEY (department_id)
+)
+ENGINE = INNODB ;
+
+CREATE TABLE zstore.positions (
+  position_id int(11) NOT NULL AUTO_INCREMENT,
+  position_name varchar(255) NOT NULL,
+  details text NOT NULL,
+  PRIMARY KEY (position_id)
+)
+ENGINE = INNODB;
+
+INSERT INTO `metadata` (`meta_type`, `description`, `meta_name`, `menugroup`, `disabled`) VALUES( 1, 'Офісний документ', 'OfficeDoc', '', 0);
+INSERT INTO `metadata` (`meta_type`, `description`, `meta_name`, `menugroup`, `disabled`) VALUES( 3, 'Офіс', 'Office', '', 0);
+
+
+
+
+
+
  
