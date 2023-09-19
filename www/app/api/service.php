58ee7d5b34c8f2b9b9da4f68c1878e50dcbf7630
leon.mbs@gmail.com <leon.mbs@gmail.com>
2023-09-19 21:42:26 +0300

    FIX  API

---
+++ www/app/api/service.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\API;
+
+use App\Helper as H;
+
+
+class service extends JsonRPC
+{
+ 
+    public function list() {
+        $list = array();
+
+        foreach (\App\Entity\Service::findYield('', 'service_name') as $ser) {
+
+            $c = array(
+                'service_id'   => $ser->service_id,
+                'service_name' => $ser->service_name ,
+                'category' => $ser->category
+          
+            );
+
+            $list[] = $c;
+        }
+
+        return $list;
+    }
+
+}
