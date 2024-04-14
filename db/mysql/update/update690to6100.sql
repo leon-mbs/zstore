ba646a24017237102bfd5b85861ed9aa29e8e62e
leon.mbs@gmail.com <leon.mbs@gmail.com>
2024-04-14 20:31:11 +0300

    fix

--- db/mysql/update/update690to6100.sql
+++ db/mysql/update/update690to6100.sql
@@ -2,6 +2,8 @@
 INSERT INTO `metadata` (`meta_type`, `description`, `meta_name`, `menugroup`, `disabled`) VALUES( 1, 'Офiсний документ', 'OfficeDoc', '', 0);
 INSERT INTO `metadata` (`meta_type`, `description`, `meta_name`, `menugroup`, `disabled`) VALUES( 3, 'Офiс', 'OfficeList', '', 0);
 
- 
+
+ALTER TABLE `documents` ADD INDEX (`priority`);
+    
 delete  from  options where  optname='version' ;
-insert  into options (optname,optvalue) values('version','6.9.5'); 
+insert  into options (optname,optvalue) values('version','6.10.0'); 
