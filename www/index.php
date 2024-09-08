9f44ea67cc8ad311d8a357c587d222873cf6182c
leon.mbs@gmail.com <leon.mbs@gmail.com>
2024-09-08 17:40:40 +0300

    fix  отчет  о  продажах

--- www/index.php
+++ www/index.php
@@ -2,7 +2,7 @@
 
 require_once 'init.php';
 
-global $_config;
+
 
 if (strpos($_SERVER['REQUEST_URI'], 'index.php') > 1) {
     die('Сайт розміщено не в кореневій папці');
@@ -13,7 +13,7 @@ try {
 
     if (($_COOKIE['remember'] ?? false) && \App\System::getUser()->user_id == 0) {
         $arr = explode('_', $_COOKIE['remember']);
-        //   $_config = parse_ini_file(_ROOT . 'config/config.ini', true);
+
         if ($arr[0] > 0 && $arr[1] === md5($arr[0] . \App\Helper::getSalt())) {
             $user = \App\Entity\User::load($arr[0]);
         }
