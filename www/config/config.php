1e60a091924986c2ae7ab4a45387213e05aa218c
leon.mbs@gmail.com <leon.mbs@gmail.com>
2024-11-14 20:08:24 +0200

    fix

--- www/config/config.php
+++ www/config/config.php
@@ -6,14 +6,13 @@ $_config = array('common'=>[],'db'=>[],'smtp'=>[]) ;
 $_config['common']['timezone'] = 'Europe/Kiev'  ;
 $_config['common']['loglevel'] = 100  ;//DEBUG = 100,INFO = 200,WARNING = 300,ERROR = 400;
 
+//соединение с  сервером  БД
 $_config['db']['host'] = 'localhost:3306'  ;
 $_config['db']['name'] = 'zstore'  ;
 $_config['db']['user'] = 'root'  ;
 $_config['db']['pass'] = 'root'  ;
 
- 
-
-
+//параметры  почтового сервера
 $_config['smtp']['usesmtp'] = false ; //якщо false використовується sendmail. Заповнюється лише поле user, ящиком з котрого надсилає sendmail
 $_config['smtp']['host'] = 'smtp.google.com' ;
 $_config['smtp']['port'] = 587 ;
