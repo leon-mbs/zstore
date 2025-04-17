2b73809038c1336f3a8b237b3d1195e634a55fe9
leon.mbs@gmail.com <leon.mbs@gmail.com>
2025-04-17 17:22:30 +0300

    fix  sql

--- db/update/temp.sql
+++ db/update/temp.sql
@@ -46,51 +46,9 @@ FROM ((prodstage ps
   JOIN parealist pa
     ON ((pa.pa_id = ps.pa_id))) ;  
  
-
-CREATE TABLE  prodentry (
-  id int NOT NULL AUTO_INCREMENT,
-  item_id int NOT NULL,
-  pa_id int   NULL,
  
-  quantity decimal(11, 3) DEFAULT 0,
-  partion decimal(10, 2) DEFAULT 0,
-  document_id int NOT NULL,
-  KEY (pa_id) ,
-  KEY (item_id) ,
-  KEY (document_id) ,
-  PRIMARY KEY (id)
-) ENGINE = INNODB DEFAULT CHARSET = utf8 ;   
   
-  
-
-CREATE
-VIEW prodentry_view
-AS
-SELECT
-  p.id AS id,
-  p.item_id AS item_id,
-  p.pa_id AS pa_id,
-  d.document_date AS document_date,
-  p.quantity AS quantity,
-  p.partion AS partion,
-  p.document_id AS document_id,
-  d.document_number AS document_number,
-  d.notes AS notes ,
-  i.itemname,
-  i.item_code,
-  a.pa_name
-FROM prodentry p
-  JOIN documents d
-    ON p.document_id = d.document_id
-  JOIN items i
-    ON p.item_id = i.item_id
-  LEFT JOIN parealist a
-    ON p.pa_id = a.pa_id   ;
-  
-  
-  INSERT INTO metadata (meta_type, description, meta_name, menugroup, disabled) VALUES( 1, 'Брак та вiдходи', 'ProdLost', 'Виробництво', 0);
   INSERT INTO metadata (meta_type, description, meta_name, menugroup, disabled) VALUES( 1, 'Перемiщення мiж етапами', 'ProdMove', 'Виробництво', 0);
-  отчет по  движению
   
   delete from options where  optname='version' ;
   insert into options (optname,optvalue) values('version','6.14.0''); 
