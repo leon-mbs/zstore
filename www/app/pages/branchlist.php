9a41fccf10ee093e83f00617d91cb2be3cda2e80
leon.mbs@gmail.com <leon.mbs@gmail.com>
2025-06-04 16:53:31 +0300

    перевод на  филиал

--- www/app/pages/branchlist.php
+++ www/app/pages/branchlist.php
@@ -9,6 +9,7 @@ use App\Helper as H;
 use Zippy\Html\DataList\DataView;
 use Zippy\Html\Form\Button;
 use Zippy\Html\Form\CheckBox;
+use Zippy\Html\Form\DropDownChoice;
 use Zippy\Html\Form\Form;
 use Zippy\Html\Form\SubmitButton;
 use Zippy\Html\Form\TextArea;
@@ -32,17 +33,25 @@ class BranchList extends \App\Pages\Base
 
 
         $this->add(new Panel('branchtable'))->setVisible(true);
-        $this->branchtable->add(new DataView('branchlist', new \ZCL\DB\EntityDataSource('\App\Entity\Branch', '', 'disabled asc,branch_name asc'), $this, 'branchlistOnRow'))->Reload();
+        $this->branchtable->add(new DataView('branchlist', new \ZCL\DB\EntityDataSource('\App\Entity\Branch', '', 'disabled asc,branch_name asc'), $this, 'branchlistOnRow'));
         $this->branchtable->add(new ClickLink('addnew'))->onClick($this, 'addOnClick');
+     
+
         $this->add(new Form('branchdetail'))->setVisible(false);
         $this->branchdetail->add(new TextInput('editbranchname'));
-
         $this->branchdetail->add(new TextInput('editaddress'));
         $this->branchdetail->add(new TextInput('editphone'));
         $this->branchdetail->add(new TextArea('editcomment'));
         $this->branchdetail->add(new CheckBox('editdisabled'));
         $this->branchdetail->add(new SubmitButton('save'))->onClick($this, 'saveOnClick');
         $this->branchdetail->add(new Button('cancel'))->onClick($this, 'cancelOnClick');
+        
+        $this->branchtable->add(new Form('formmove')) ;        
+        $this->branchtable->formmove->add(new DropDownChoice('selmove'));
+        $this->branchtable->formmove->add(new SubmitButton('btnmove'))->onClick($this, 'moveOnClick');
+
+        
+        $this->update();
     }
 
     public function branchlistOnRow($row) {
@@ -66,7 +75,7 @@ class BranchList extends \App\Pages\Base
             $this->setError($del);
             return;
         }
-        $this->branchtable->branchlist->Reload();
+        $this->update();
     }
 
     public function editOnClick($sender) {
@@ -107,15 +116,66 @@ class BranchList extends \App\Pages\Base
         $this->_branch->comment = $this->branchdetail->editcomment->getText();
         $this->_branch->disabled = $this->branchdetail->editdisabled->isChecked() ? 1 : 0;
 
-        $this->_branch->Save();
+        $this->_branch->save();
         $this->branchdetail->setVisible(false);
         $this->branchtable->setVisible(true);
-        $this->branchtable->branchlist->Reload();
+        $this->update();
     }
 
     public function cancelOnClick($sender) {
         $this->branchtable->setVisible(true);
         $this->branchdetail->setVisible(false);
+    }
+  
+    private function update() {
+        $this->branchtable->branchlist->Reload();
+        
+        $list=Branch::findArray("branch_name", "disabled <>1", "branch_name") ;
+        
+        $this->branchtable->formmove->selmove->setOptionList($list);  
+        $this->branchtable->formmove->selmove->setValue(0);  
+        
+    }
+    public function moveOnClick($sender) {
+        $id= intval($this->branchtable->formmove->selmove->getValue()); 
+        if($id==0) {
+            $this->setError('Не вибрана  фiлiя');
+            return;    
+        }
+        $conn = \ZDB\DB::getConnect();
+        $conn->BeginTrans();
+        try {
+ 
+            $conn->Execute("update documents set branch_id={$id} where  coalesce(branch_id,0) = 0 ");
+            $conn->Execute("update stores set branch_id={$id} where  coalesce(branch_id,0) = 0 ");
+            $conn->Execute("update employees set branch_id={$id} where  coalesce(branch_id,0) = 0 ");
+            $conn->Execute("update equipments set branch_id={$id} where  coalesce(branch_id,0) = 0 ");
+            $conn->Execute("update mfund set branch_id={$id} where  coalesce(branch_id,0) = 0 ");
+            $conn->Execute("update parealist set branch_id={$id} where  coalesce(branch_id,0) = 0 ");
+            $conn->Execute("update poslist set branch_id={$id} where  coalesce(branch_id,0) = 0 ");
+            $conn->Execute("update timesheet set branch_id={$id} where  coalesce(branch_id,0) = 0 ");
+            
+            
+            $conn->CommitTrans();
+            
+            $this->setSuccess('Виконано');
+  
+        } catch(\Throwable $ee) {
+            global $logger;
+            $conn->RollbackTrans();
+  
+            $this->setError($ee->getMessage());
+            $logger->error( $ee->getMessage()  );
+
+
+        }        
+        
+        
+    
+       $this->branchtable->formmove->selmove->setValue(0);  
+       
+       
+         
     }
 
 }
