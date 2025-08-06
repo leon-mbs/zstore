45b3668a36b8fe14f392dcec960c8257abbc392c
leon.mbs@gmail.com <leon.mbs@gmail.com>
2025-08-06 23:00:46 +0300

    6.15.0 beta

--- www/app/pages/firm.php
+++ www/app/pages/firm.php
@@ -40,6 +40,8 @@ class Firm extends \App\Pages\Base
         $this->firmform->add(new TextInput('address',$firm['address']));
         $this->firmform->add(new TextInput('city',$firm['address']));
         $this->firmform->add(new TextInput('vdoc',$firm['vdoc']));
+        $this->firmform->add(new CheckBox('isjur',$firm['isjur']??0));
+        $this->firmform->add(new CheckBox('usetax',$firm['usetax']??0));
 
         $this->firmform->add(new TextInput('sign',$firm['sign']));
         $this->firmform->add(new TextInput('stamp',$firm['stamp']));
@@ -68,6 +70,8 @@ class Firm extends \App\Pages\Base
         $firm['phone'] = $this->firmform->phone->getText();
         $firm['address'] = $this->firmform->address->getText();
         $firm['vdoc'] = $this->firmform->vdoc->getText();
+        $firm['isjur'] = $this->firmform->isjur->isChecked() ? 1:0;
+        $firm['usetax'] = $this->firmform->usetax->isChecked() ? 1:0;
 
         $firm['sign'] = $this->firmform->sign->getText();
         $firm['stamp'] = $this->firmform->stamp->getText();
