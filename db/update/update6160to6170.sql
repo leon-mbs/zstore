3b1d13c4623958cb84bc7bc416a6efe73d4b8d2e
leon.mbs@gmail.com <leon.mbs@gmail.com>
2025-10-01 21:24:35 +0300

    save

---
+++ db/update/update6160to6170.sql
@@ -0,0 +1,9 @@
+
+    
+INSERT INTO metadata (meta_type, description, meta_name,  menugroup,   disabled) VALUES(  3, 'Замiни ТМЦ', 'Substiturion', 'Склад',     0);
+UPDATE metadata set menugroup ='Каса та платежі' where meta_name='EndDay';
+    
+ 
+
+delete from options where  optname='version' ;
+insert into options (optname,optvalue) values('version','6.17.0'); 
