ed28f7856d845df13cd976fba8e2fbb1f6cc5cd6
leon.mbs@gmail.com <leon.mbs@gmail.com>
2025-11-25 23:03:34 +0200

    6.18.0beta

--- www/app/system.php
+++ www/app/system.php
@@ -10,9 +10,9 @@ use App\Entity\User;
  */
 class System
 {
-    public const CURR_VERSION = "6.17.2";
-    public const PREV_VERSION = "6.17.1";
-    public const REQUIRED_DB  = "6.17.0";
+    public const CURR_VERSION = "6.18.0";
+    public const PREV_VERSION = "6.17.2";
+    public const REQUIRED_DB  = "6.18.0";
    
 
     /**
