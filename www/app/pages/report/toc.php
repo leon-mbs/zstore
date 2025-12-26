3a7624f1e54edc1775b27cf408aad685a6ae3e6f
leon.mbs@gmail.com <leon.mbs@gmail.com>
2025-12-26 14:26:14 +0200

    801RC

--- www/app/pages/report/toc.php
+++ www/app/pages/report/toc.php
@@ -2,17 +2,17 @@
 
 namespace App\Pages\Report;
 
-use App\Helper as H;
+ 
 use Zippy\Html\Form\Date;
 use Zippy\Html\Form\DropDownChoice;
 use Zippy\Html\Form\Form;
 use Zippy\Html\Form\TextInput;
 use Zippy\Html\Form\SubmitButton;
 use Zippy\Html\Label;
-use Zippy\Html\Link\RedirectLink;
 use Zippy\Html\Link\ClickLink;
 use Zippy\Html\Panel;
-
+use App\Entity\Doc\Document;
+use App\Helper as H;
  
 class Toc extends \App\Pages\Base
 {
@@ -55,18 +55,80 @@ class Toc extends \App\Pages\Base
  
 
         $period = (int)$this->filter->period->getValue();
+        $period= 24;
         $end=strtotime('-7 day') ;
-        $start=strtotime("-{$period} month",$start) ;
+        $start=strtotime("-{$period} month",$end) ;
       
         $from = $conn->DBDate($start); 
         $to = $conn->DBDate($end); 
         
-           //Актуальність складів
+        $dd = "document_date >={$from} and document_date <={$to} ";
+        
+           //актуальность складов
         $detail1=[] ;
-      
+        $items=[];
+        $itemsd=[];
+        
+         
+        foreach(Document::findYield($dd." and meta_name='Order' and state >4 ") as $order){
+             foreach ($order->unpackDetails('detaildata') as $item) {
+                 if(!isset($items[$item->item_id])) {
+                    $items[$item->item_id] = ['name'=> $item->itemname,'code'=>$item->item_code??'','amount'=>0];
+                 }
+                 
+                 $sql="select coalesce(sum(quantity),0)  as totqty  from entrylist_view where item_id={$item->item_id} and document_date <=  ". $conn->DBDate($order->document_date) ;
+                 $onstore= doubleval($conn->GetOne($sql) );
+                 if($onstore < $item->quantity && $item->quantity >0)  {
+                     $items[$item->item_id]['amount'] += ($item->quantity - $onstore) * $item->price ; 
+                 }
+                 
+                 
+             }
+             $logs = $order->getLogList([9,11,14,20]);
+             if(count($logs)>0) {
+                 $last= array_shift($logs);
+              //   $dt = strtotime("+1 day",  $order->document_date);
+                $datetime1 = new \DateTime( date('Y-m-d',$order->document_date)  );
+                $datetime2 = new \DateTime( date('Y-m-d',$last->createdon));
+                $interval = $datetime1->diff($datetime2);
+                $days = $interval->days;
+                                               
+                if($days > 1) { // обрабатывался  больше двух дней
+                     foreach ($order->unpackDetails('detaildata') as $item) {
+                         if(!isset($itemsd[$item->item_id])) {
+                            $itemsd[$item->item_id] = ['name'=> $item->itemname,'code'=>$item->item_code??'','days'=>[]];
+                         }
+                         
+                         $itemsd[$item->item_id]['days'][]=$days ; 
+                        
+                         
+                         
+                     }
+                 }
+                 
+             }
+        }
+        foreach($items as $item){
+           $item['amount'] = H::fa($item['amount']); 
+           $detail1[]=$item; 
+        }
+        foreach($itemsd as $item){
+           $avg = array_sum($item['days'])/count($item['days']);  
+            
+           $item['days'] = number_format($avg, 1, '.', '') ; 
+           $detail2[]=$item; 
+        }
+       
+        
+         //товары с задерэкой после  заказа
+         //задержка с заявки до получения
+         //задерэеки по  поставщиком  после  оплат        
+              
         $header = array(
            "_detail1" => $detail1,
-           "isdetail1" => count($detail1) > 0
+           "isdetail1" => count($detail1) > 0 ,
+           "_detail2" => $detail2,
+           "isdetail2" => count($detail2) > 0 
 
 
                         
--- www/templates/printforms/report/toc.tpl
+++ www/templates/printforms/report/toc.tpl
@@ -1,40 +1,70 @@
 <table class="ctable" cellspacing="0" cellpadding="1">
     <tr    >
-        <td align="center" colspan="5">
+        <td align="center" colspan="3">
             <h4  >Обмеження системи </h4>
         </td>
     </tr>
  
     {{#isdetail1}}
   <tr    >
-        <td  >
+        <td  colspan="3"  >
             <b  >Актуальність складів  </b>   
         </td>
     </tr>    
  <tr  >
-        <td  >
+        <td  colspan="3"  >
             <small  > Товари, яких не виявилось на складі на момент замовлення </small>   
         </td>
     </tr>  
    <tr>
        
-        <td   style="border: solid black 1px"  >Товар </td>
- 
-        <td   style="border: solid black 1px" align="right">На суму </td>
+        <th   style="border: solid black 1px"  >Товар </th>
+        <th   style="border: solid black 1px"  >Код </th>
+        <th   style="border: solid black 1px" align="right">На суму </th>
 
     </tr>      
     {{#_detail1}}
     <tr>
        
-        <td    >{{item_name}} </td>
-        <td    align="right">{{days}} </td>
+        <td    >{{ name}} </td>
+        <td    >{{code}} </td>
+        <td    align="right">{{amount}} </td>
 
     </tr>
     {{/_detail1}}
     
     {{/isdetail1}}
  
+ 
+    {{#isdetail2}}
+  <tr    >
+        <td  colspan="3"  >
+           <br> <b  >Затримка відправки   </b>   
+        </td>
+    </tr>    
+ <tr  >
+        <td  colspan="3"  >
+            <small  >Середня затримка  відправки товару по замовленню. <br>Для замовлень з різницею між датою замовлення і  відправкою  більше двох днів   </small>   
+        </td>
+    </tr>  
+   <tr>
+       
+        <th   style="border: solid black 1px"  >Товар </th>
+        <th   style="border: solid black 1px"  >Код </th>
+        <th   style="border: solid black 1px" align="right">Дні </th>
 
+    </tr>      
+    {{#_detail2}}
+    <tr>
+       
+        <td    >{{ name}} </td>
+        <td    >{{code}} </td>
+        <td    align="right">{{days}} </td>
+
+    </tr>
+    {{/_detail2}}
+    
+    {{/isdetail2}}
 </table>
 
 
